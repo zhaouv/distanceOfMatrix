c7becba19eedd3ea85b798368eb19ffd56a3166a
YouWei Zhao <zhao.uv@gmail.com>
2020-08-03 20:27:02 +0800

    u

--- distanceOfMatrix.jl
+++ distanceOfMatrix.jl
@@ -1,5 +1,5 @@
 
-function multiply(a::Array{Int,1}, b::Array{Int,1}, mod::Int, r::Array{Int,1})
+function multiply!(a::Array{Int,1}, b::Array{Int,1}, mod::Int, r::Array{Int,1})
     x1::Int = (a[1] + a[4]) * (b[1] + b[4])
     x2::Int = (a[3] + a[4]) * b[1]
     x3::Int = a[1] * (b[2] - b[4])
@@ -13,46 +13,43 @@ function multiply(a::Array{Int,1}, b::Array{Int,1}, mod::Int, r::Array{Int,1})
     r[4] = (x1 + x3 - x2 + x6)%mod
 end
 
-m=[[1, 1, 0, 1], [1, 0, 1, 1]]
-
-function backtrack!(t::Int, mod::Int, wlength::Int, x::Array{Int,1}, ce::Array{Array{Int,1},1}, result::Bool) 
-    if result 
-        return result
-    end
-    if t >= wlength 
-        e = ce[t]
-        if ((e[1] == 1 && e[2] == 0 && e[3] == 0 && e[4] == 1) || (e[1] == mod - 1 && e[2] == 0 && e[3] == 0 && e[4] == mod - 1)) 
-            result = true
-        end
-    else 
-        for i in [0,1]
-            x[t+1] = i;
-            mi = m[i+1]
-            if t==0
-                ce[t+1] = mi
-            else
-                multiply(mi, ce[t], mod, ce[t+1])
-            end
-            result = backtrack!(t + 1, mod, wlength, x, ce, result);
-            if result 
-                return result
-            end
-        end
-    end
-    return result
-end
-
 function process(mod::Int) 
     println(mod)
+    m=[[1, 1, 0, 1], [1, 0, 1, 1]]
     wlength::Int = 0
     while true
         wlength+=1
-
         x = Int[0 for i in 1:wlength]
         ce = [Int[0 for j in 1:4] for i in 1:wlength]
         result = false
+        function backtrack(t::Int, mod::Int, wlength::Int) 
+            if result
+                return 
+            end
+            if t >= wlength 
+                e = ce[t]
+                if ((e[1] == 1 && e[2] == 0 && e[3] == 0 && e[4] == 1) || (e[1] == mod - 1 && e[2] == 0 && e[3] == 0 && e[4] == mod - 1)) 
+                    result = true
+                end
+            else 
+                for i in [0,1]
+                    x[t+1] = i;
+                    mi = m[i+1]
+                    if t==0
+                        ce[t+1] = mi
+                    else
+                        multiply!(mi, ce[t], mod, ce[t+1])
+                    end
+                    backtrack(t + 1, mod, wlength);
+                    if result
+                        return 
+                    end
+                end
+            end
 
-        result = backtrack!(0, mod, wlength, x, ce, result)
+        end
+
+        backtrack(0, mod, wlength)
 
         if result
             if wlength < mod
@@ -67,7 +64,7 @@ function process(mod::Int)
                 println("]")
             end
         end
-        if result 
+        if result
             break
         end
     end
